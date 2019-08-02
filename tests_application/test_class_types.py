e0d5ae63631e281c7f919c507b1feb8240799270
Daryl Stark <daryl@dstark.nl>
2019-08-03 00:50:54 +0200

    Created a few unit tests to check classtypes

---
+++ tests_application/test_class_types.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python3
+#---------------------------------------------------------------------------------------------------
+# Import the pytest module for the testing
+import pytest
+#---------------------------------------------------------------------------------------------------
+# Set the correct path to the 
+import sys
+sys.path.append("src-new/")
+#---------------------------------------------------------------------------------------------------
+from me import *
+from me_database import *
+from template_loader import *
+#---------------------------------------------------------------------------------------------------
+# TODO:
+# Create a decorator for the test-methods so the boilerplate code can be reduced
+#---------------------------------------------------------------------------------------------------
+class TestClassTypes:
+    """ Unit testing class to test the types of classes in the application. There are a few types of
+        classes:
+        - Classes can be normal
+        - Some classed need to be a singleton, meaning only one instance of the class can exist)
+        - Some classes need to be static, meaning no instances of it exists
+        - Some classes are abstract, meaning they can only be used as base class
+        - Some classes are exception classes, meaning they can be used as Exceptions
+        This Test Class tests if all the classes are from the correct type """
+    
+    def test_normal_classes(self):
+        """ Method to test all normal classes """
+
+        # Set the classes to test
+        classes = [
+            PageAPIAAA, PageAPIEvents, PageAPIFeed, PageAPIUsers,
+            PageAPI, PageMain, PageUI, EventChange,
+            EventSyncResult, Event, FeedItemEventChange, FeedItem,
+            Filter, Stage, User, Venue
+        ]
+
+        # Empty list of errourness classes
+        errors = {}
+        
+        # Loop through the classes and test if it is possible to create an instance from it, a
+        # second instance and if they are different from eachother
+        for class_ in classes:
+            try:
+                # Create two instances of the class to check if it is possible to create instances
+                # of it
+                obj1 = class_()
+                obj2 = class_()
+
+                # Check if they are the same object. If they are the same, they are not correct so
+                # we throw an error
+                if obj1 is obj2:
+                    raise Exception('Instances of class "{name}" "obj1" and "obj2" are the same object ({id})'.format(
+                        name = class_.__name__,
+                        id = id(obj1)
+                    ))
+            except Exception as err_message:
+                errors[class_.__name__] = err_message
+        
+        # Check if we have errors. If we have, raise and False assertion so the check fails.
+        # Otherwise, raise an True assertion for a succesfull test
+        if len(errors) == 0:
+            assert True
+        else:
+            # Create an string with the errors
+            error_string = ', '.join([ 'Class "{class_}": "{error}"'.format(class_ = class_, error = error) for class_, error in errors.items() ])
+            assert False, error_string
+    
+    def test_singleton_classes(self):
+        """ Method to test singleton classes """
+
+        # TODO: Write this. Not yet used though
+
+        classes = []
+        assert True
+    
+    def test_static_classes(self):
+        """ Method to test static classes """
+
+        # Set the classes to test
+        classes = [
+            Me, Database, TemplateLoader
+        ]
+
+        # Empty list of errourness classes
+        errors = {}
+
+        # Loop through the classes and test if it is possible to create an instance from it. If it
+        # is, the test should fail
+        for class_ in classes:
+            try:
+                obj1 = class_()
+            except Exception as err_message:
+                # If we receive an error, the class is correct
+                pass
+            else:
+                errors[class_.__name__] = 'Could create an instance of this static class'
+
+        # Check if we have errors. If we have, raise and False assertion so the check fails.
+        # Otherwise, raise an True assertion for a succesfull test
+        if len(errors) == 0:
+            assert True
+        else:
+            # Create an string with the errors
+            error_string = ', '.join([ 'Class "{class_}": "{error}"'.format(class_ = class_, error = error) for class_, error in errors.items() ])
+            assert False, error_string
+    
+    def test_abstract_classes(self):
+        """ Method to test static classes """
+
+        # Set the classes to test
+        classes = [
+            APIPage, Page
+        ]
+        
+        # Empty list of errourness classes
+        errors = {}
+
+        # Loop through the classes and test if it is possible to create an instance from it. If it
+        # is, the test should fail
+        for class_ in classes:
+            try:
+                obj1 = class_()
+            except Exception as err_message:
+                # If we receive an error, the class is correct
+                pass
+            else:
+                errors[class_.__name__] = 'Could create an instance of this abstract class'
+
+        # Check if we have errors. If we have, raise and False assertion so the check fails.
+        # Otherwise, raise an True assertion for a succesfull test
+        if len(errors) == 0:
+            assert True
+        else:
+            # Create an string with the errors
+            error_string = ', '.join([ 'Class "{class_}": "{error}"'.format(class_ = class_, error = error) for class_, error in errors.items() ])
+            assert False, error_string
+    
+    def test_exception_classes(self):
+        """ Method to test if a class is a exception class """
+
+        # Set the classes to test
+        classes = [
+            MeException, MeAmbigiousPathException, MeRegexException, MeAbigiousURLNameException,
+            MeConfigFileException, MeConfigException, MeEnvironmentException, MeAPIGroupNotRegisteredException,
+            MeAPINoAPIGroupException, MeValueException, MeAPIUnsupportedFormatException, MeAPIEndPointInvalidException,
+            MeAPINoEndPointException, MeAPIInvalidReturnException, TemplateLoaderException, TemplateNotFoundException
+        ]
+
+        # Empty list of errourness classes
+        errors = {}
+
+        # Loop through the classes and test if it is possible to create an instance from it. If it
+        # is, the test should fail
+        for class_ in classes:
+            try:
+                if not isinstance(class_(), Exception):
+                    raise Exception('Not an exception class')
+            except Exception as err_message:
+                errors[class_.__name__] = err_message
+        
+        # Check if we have errors. If we have, raise and False assertion so the check fails.
+        # Otherwise, raise an True assertion for a succesfull test
+        if len(errors) == 0:
+            assert True
+        else:
+            # Create an string with the errors
+            error_string = ', '.join([ 'Class "{class_}": "{error}"'.format(class_ = class_, error = error) for class_, error in errors.items() ])
+            assert False, error_string
+#---------------------------------------------------------------------------------------------------
