de874ba86ac113245dd67db4f616366267f61374
Daryl Stark <daryl@dstark.nl>
2019-08-26 21:18:32 +0200

    Started with defining pages

--- src-new/html/protected-js/ui.js
+++ src-new/html/protected-js/ui.js
@@ -5,10 +5,15 @@ class UI {
     // Static class for the UI of the application. Does all basic functions, like setting the
     // listeners and starting specific pages.
 
+    // The classes to start when the user starts a specific page
+    static page_classes = {
+        "main_feed": "test"
+    };
+
     static init() {
         // Initiator of the User Interface. Sets all listeners where they should be
 
-        // Set a listener to the 'main menu' headers to hide the menu
+        // Set a listener to the 'main menu' headers to hide the submenus
         $('#main_menu').children('nav').each(function(element, value) {
             // Set the listener to the button
             $(value).find('button').click(function() {
@@ -23,6 +28,14 @@ class UI {
                 // Slide the menu up or down, depending on the current state
                 $(value).find('nav#' + $(value).attr('id')).slideToggle(100);
             });
+        });
+
+        // Set listeners to the 'main menu' menuitems themselfs
+        $('#main_menu').find('a').click(function() {
+            var menu_item_id = $(this).attr('id');
+            var page_class = UI.page_classes[menu_item_id];
+
+            console.log(page_class);
         });
     }
 }
